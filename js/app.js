68d7e3588e3a70065603e15892ae3d99cb6e4b1a
Keith Kelly <keithkelly31@gmail.com>
2016-12-17 14:32:47 -0500

    Refactoring
    - Changed hiding from adding and removing classes to changing the
    elements style
    - Made variable names more descriptive
    - Started exceeds criteria by building and adding search elements

--- js/app.js
+++ js/app.js
@@ -1,88 +1,97 @@
-// Get needed DOM elements
-var studentList = document.querySelector('.student-list');
+var studentsListContainer = document.querySelector('.student-list');
+var numberOfResultsToShow = 10;
+var studentsList = studentsListContainer.getElementsByClassName('student-item');
+var totalListPages = Math.ceil(studentsList.length / numberOfResultsToShow);
 
-// Determine studentCount and totalPages
-var resultsToShow = 10;
-var students = studentList.getElementsByClassName('student-item');
-var totalPages = Math.ceil(students.length / resultsToShow);
 
-// Show initial batch of students
-var initialStudentsList = function() {
-	var end = 0 + resultsToShow - 1;
-	for(var i = 0; i < students.length; i++) {
-		if(i > end) {
-			students[i].classList.add('hidden');
+/** @function
+ * Show a list of students based on a specified starting point.
+ * Hides any students that fall outside of the start and end indexes.
+ * @param {number} start The starting index point
+ */
+var showStudents = function(startingIndex) {
+	var endingIndex = startingIndex + numberOfResultsToShow - 1;
+
+	// Loop through items and hide items that should not be shown
+	for(var i = 0; i < studentsList.length; i++) {	
+		// Initially remove any style elements if they were set		
+		studentsList[i].removeAttribute('style');					
+		
+		if(i < startingIndex || i > endingIndex) {										
+			studentsList[i].style.display = 'none';
 		}
 	}
 }
 
-// Remove active classes from all pagination buttons
-var removeClass = function(elementClass) {
-	var elements = document.getElementsByClassName(elementClass);
-	while(elements.length) {
-		elements[0].classList.remove(elementClass);
+
+/** @function
+ * Change the results shown when a button is clicked.
+ */
+var updateStudentList = function() {
+	var pageNumber = parseInt(this.innerText, 10);
+	var startingIndex = numberOfResultsToShow * pageNumber - numberOfResultsToShow;
+	var activeElements = document.getElementsByClassName('active');
+
+	// Remove active class from other pagination buttons
+	while(activeElements.length) {
+		activeElements[0].classList.remove('active');
 	}
-}
 
-// Show specific results based on starting point in list
-var showResults = function(start) {
-	var end = start + resultsToShow - 1;
-
-	// Remove all other active classes
-	removeClass('hidden');
-
-	// Loop through items and add hidden class to items not to show
-	for(var i = 0; i < students.length; i++) {
-		if(i < start || i > end) {
-			students[i].classList.add('hidden');
-		}
-	}
-}
-
-// Change results when button is clicked
-var changeResults = function() {
-	var page = parseInt(this.innerText, 10);
-	var start = resultsToShow * page - resultsToShow;
-
-	// Remove all other active classes
-	removeClass('active');
-
-	// Add the class active to the current item
 	this.classList.add('active');
 
-	// Show results
-	showResults(start);
+	showStudents(startingIndex);
 }
 
-// Generate pagination buttons
-var addPagination = function() {
-	// Generate needed elements
-	var container = document.createElement('div');
-	var unorderedList = document.createElement('ul');
 
-	// Add classes to new elements
-	container.classList.add('pagination');
+/** @function
+ * Geneate pagination buttons.
+ */
+var addPaginationButtons = function() {
+	var paginationContainer = document.createElement('div');
+	var paginationButtonsGroup = document.createElement('ul');
+	paginationContainer.classList.add('pagination');
 
 	// Generate each pagination list item
-	for(var i = 1; i <= totalPages; i++) {
-		var listItem = document.createElement('li');
-		var anchor = document.createElement('a');
-		anchor.setAttribute('href', '#');
-		listItem.append(anchor);
-		anchor.innerText = i;
-		anchor.onclick = changeResults;
+	for(var i = 1; i <= totalListPages; i++) {
+		var paginationButton = document.createElement('li');
+		var buttonLink = document.createElement('a');
+
+		buttonLink.setAttribute('href', '#');
+		buttonLink.innerText = i;
+		buttonLink.onclick = updateStudentList;
+
+		// Initially set the first button as active
 		if(i === 1) {
-			anchor.classList.add('active');
+			buttonLink.classList.add('active');
 		}
-		unorderedList.append(listItem);
+
+		paginationButton.append(buttonLink);
+		paginationButtonsGroup.append(paginationButton);
 	}
 
-	// Append unordered list to pagination container
-	container.append(unorderedList);
-
-	// Insert pagination buttons after student list
-	studentList.insertAdjacentElement('afterend', container);
+	paginationContainer.append(paginationButtonsGroup);
+	studentsListContainer.insertAdjacentElement('afterend', paginationContainer);
 }
 
-initialStudentsList();
-addPagination();
+/** @function
+ * Geneate search form.
+ */
+var addSearchForm = function() {
+	var pageHeader = document.querySelector('.page-header');
+	var searchContainer = document.createElement('div');
+	var searchInput = document.createElement('input');
+	var searchButton = document.createElement('button');
+
+	searchContainer.classList.add('student-search');
+	searchInput.setAttribute('placeholder', 'Search for students...');
+	searchButton.innerText = 'Search';
+
+	searchContainer.append(searchInput);
+	searchContainer.append(searchButton);
+
+	pageHeader.append(searchContainer);
+}
+
+showStudents(0);
+addPaginationButtons();
+addSearchForm();
